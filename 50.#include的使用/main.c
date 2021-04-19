e1e65f2e801bb2f28f61ad2b73a1a18f94717836
wjp578 <2923525168@qq.com>
2021-04-19 23:52:41 +0800

    4.19

---
+++ 50.#include的使用/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "print.h"
+
+int main()
+{
+    print();
+    return 0;
+}
---
+++ 50.#include的使用/print.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "print.h"
+
+void print()
+{
+    printf("Hello world!\n");
+}
