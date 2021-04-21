7868ac985b5445405eb6245a6344b362f04daf97
wjp578 <2923525168@qq.com>
2021-04-21 21:56:05 +0800

    4.21

---
+++ 71.编写input()和output()函数输入，输出5个学生的数据记录/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//编写input()和output()函数输入，输出5个学生的数据记录。
+
+typedef struct Student{
+    char name[20];
+    char sex[5];
+    int  age;
+}Stu;
+
+void input(Stu *stu)
+{
+    int i;
+    for(i=0;i<5;i++)
+        scanf("%s%s%d",stu[i].name,stu[i].sex,&(stu[i].age));
+}
+
+void output(Stu *stu)
+{
+    int i;
+    for(i=0;i<5;i++)
+        printf("%s %s %d\n",stu[i].name,stu[i].sex,stu[i].age);
+}
+
+int main()
+{
+    Stu stu[5];
+    printf("请输入5个学生的信息：姓名 性别 年龄:\n");
+    input(stu);
+    printf("5个学生的信息如下：\n姓名  性别  年龄\n");
+    output(stu);
+
+    return 0;
+}
---
+++ 72.创建一个链表。/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct ListNode
+{
+    char val;
+    struct ListNode *next;
+}LNode,*LinkNode;
+
+LinkNode creat_LNode()
+{
+    LinkNode head=(LinkNode)malloc(sizeof(LNode));//头节点，->next为第一个元素节点
+    LinkNode p=head;
+
+    char ch;
+    while(scanf("%c",&ch)!=EOF)
+    {
+        char t=getchar();//吞掉回车
+        LinkNode temp=(LinkNode)malloc(sizeof(LNode));
+        temp->val=ch;
+        p->next=temp;
+        p=temp;
+    }
+
+    p->next=NULL;
+
+    LinkNode re=head->next;
+    free(head);
+    head=NULL;
+
+    return re;
+}
+
+void print_LNode(LinkNode head)
+{
+    LinkNode cur=head;
+
+    while(cur != NULL || cur->next != NULL)
+    {
+        printf("-->%c",cur->val);
+        cur=cur->next;
+    }
+
+    printf("\n");
+}
+
+void delete_LNode(LinkNode head)
+{
+    LinkNode cur_next=NULL;
+
+    while(cur_next != NULL || cur_next->next != NULL)
+    {
+        cur_next=head->next;
+        free(head);
+        head=cur_next;
+    }
+}
+
+int main()
+{
+    LinkNode head=creat_LNode();//头指针，指向第一个元素节点
+    print_LNode(head);
+    delete_LNode(head);
+
+    return 0;
+}
